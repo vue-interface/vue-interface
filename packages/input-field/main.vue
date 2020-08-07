44f885c688fd79cfd3d2ee6ca6382cc9925a4c3e
Justin Kimbrell <jkimbrell@actengage.com>
2020-08-07 09:56:05 -0600

    pre-release commit

--- packages/input-field/main.vue
+++ packages/input-field/main.vue
@@ -1,5 +1,6 @@
 <script>
 import './src/css/InputField.css';
+import '@vue-interface/form-control/src/scss/CustomFields.scss';
 import InputField from './src/InputField';
 export default InputField;
 </script>
