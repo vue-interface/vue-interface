612b4423892b5e9c8f5a70fcfe4fe60c9bb9385e
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2020-11-11 18:46:06 -0700

    pre-release commit

--- packages/activity-indicator/babel.config.js
+++ packages/activity-indicator/babel.config.js
@@ -9,3 +9,15 @@ module.exports = {
         }]
     ]
 };
+module.exports = {
+    presets: [
+        ['@vue/app', {
+            targets: {
+                node: "current",
+                "ie": "10",
+                browsers: "> 0.25%, not dead",
+            }
+        }]
+    ]
+};
+  
