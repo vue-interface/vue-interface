3ab075d76e0e280adf460cc43db9260b03621ae7
Justin Kimbrell <jkimbrell@actengage.com>
2022-02-08 11:01:21 -0700

    pre-release commit

---
+++ packages/form-control/tailwindcss/index.js
@@ -0,0 +1,12 @@
+module.exports = [
+    require('./form-control'),
+    require('./form-check'),
+    require('./form-switch'),
+    require('./form-select'),
+    require('./form-file'),
+    require('./form-text'),
+    require('./form-labels'),
+    require('./form-group'),
+    require('./input-group'),
+    require('./validation')
+];
