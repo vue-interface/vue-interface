3aad4711fe06772827f9be3f35f9022d45f82234
Justin Kimbrell <jkimbrell@actengage.com>
2022-02-10 09:05:12 -0700

    pre-release commit

--- packages/form-control/tailwindcss/safelist.js
+++ packages/form-control/tailwindcss/safelist.js
@@ -1,5 +1,12 @@
+console.log(123);
+
 module.exports = () => ([
+    'form-check-input',
+    'form-check-label',
     'form-control',
+    'form-control-sm',
+    'form-control-lg',
+    'form-switch',
     'form-control-sm',
     'form-control-lg',
     'form-group',
