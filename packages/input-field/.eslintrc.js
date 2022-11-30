bdfae11af6b9f62746134d773fbf3682ba3730a5
Mitchell Solomon <msolomon@actengage.com>
2022-11-30 13:01:17 -0500

    eslint

--- packages/input-field/.eslintrc.js
+++ packages/input-field/.eslintrc.js
@@ -1,53 +1,38 @@
 module.exports = {
-    extends: [
-        'plugin:vue/recommended'
-    ],
-    parserOptions: {
-        'parser': 'babel-eslint'
+    root: true,
+    env: {
+        node: true
     },
-    // add your custom rules here
+    extends: [
+        'plugin:vue/vue3-recommended',
+        '@vue/eslint-config-typescript'
+    ],
     rules: {
-        'vue/no-v-html': 0,
-        'vue/require-default-prop': 0,
-        "vue/max-attributes-per-line": ["error", {
-            "singleline": 5,
-            "multiline": {
-                "max": 1,
-                "allowFirstLine": true
+        'quotes': [2, 'single', { 'avoidEscape': true }],
+        'vue/multi-word-component-names': 0,
+        'vue/html-indent': ['error', 4, {
+            'attribute': 1,
+            'baseIndent': 1,
+            'closeBracket': 1,
+            'alignAttributesVertically': false,
+            'ignores': []
+        }],
+        'vue/html-closing-bracket-newline': ['error', {
+            'singleline': 'never',
+            'multiline': 'never'
+        }],
+        'keyword-spacing': ['error', {
+            'before': true,
+            'after': true,
+            'overrides': {
+                'if': { 'after': false },
+                'while': { 'after': false },
+                'for': { 'after': false },
             }
         }],
-        "vue/html-indent": ["error", 4, {
-            "attribute": 1,
-            "baseIndent": 1,
-            "closeBracket": 1,
-            "alignAttributesVertically": false,
-            "ignores": []
-        }],
-        "vue/html-closing-bracket-newline": ["error", {
-            "singleline": "never",
-            "multiline": "never"
-        }],
-        // Ensure no space after keywords like if.
-        'keyword-spacing': ["error", {
-            "overrides": {
-                "if": { "after": false },
-                "for": { "after": false },
-                "while": { "after": false }
-            }
-        }],
-        // Use alternative brace style
-        'brace-style': ['error', 'stroustrup'],
-        // No spaces before parens
-        'space-before-function-paren': ['error', 'never'],
-        // Indent for tabs because spaces suck
         'indent': ['error', 4],
-        // Semi colons always where they need to be
-        'semi': [2,'always'],
-        // allow paren-less arrow functions
-        'arrow-parens': 0,
-        // allow async-await
-        'generator-star-spacing': 0,
-        // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+        'semi': [2, 'always'],
+        'object-curly-spacing': ['error', 'always'],
+        'space-before-blocks': ['error', 'always']
     }
 };
