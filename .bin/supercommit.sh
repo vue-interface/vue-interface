9be605f1b36b78d3d0138e6e85ad3861961f5d50
Justin Kimbrell <jkimbrell@actengage.com>
2023-06-22 12:52:46 -0600

    the super commit

--- .bin/supercommit.sh
+++ .bin/supercommit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 git submodule foreach git add --ignore-errors -A .
 git submodule foreach 'git commit -q -am "this is the super commit" || :'
+git submodule foreach git push origin master
 
 git add --ignore-errors -A .
 git commit -q -am  "this is the super commit"
+git push origin master
