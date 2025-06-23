f017448980d6f0a5928603542fdda952f1bed318
Andrew-Knt <jak@jandrewknight.com>
2025-06-23 10:41:37 -0400

    fix: Convert ActivityIndicator.vue to Vue composable API with strict typescript

--- packages/activity-indicator/src/ActivityIndicator.vue
+++ packages/activity-indicator/src/ActivityIndicator.vue
@@ -1,4 +1,91 @@
-<script lang="ts">
+<script setup lang="ts">
+import { ComponentRegistry } from '@vue-interface/component-registry';
+import { computed, inject, toRaw, type Component } from 'vue';
+import { registry } from './registry';
+
+interface ActivityIndicatorProps {
+    absolute?: boolean;
+    center?: boolean;
+    size?: string;
+    registry?: string;
+    type: Component | string;
+    height?: string | number;
+    maxHeight?: string | number;
+    minHeight?: string | number;
+    width?: string | number;
+    maxWidth?: string | number;
+    minWidth?: string | number;
+}
+
+const props = withDefaults(defineProps<ActivityIndicatorProps>(), {
+    absolute: false,
+    center: false,
+    size: 'md',
+    registry: 'indicators',
+    height: undefined,
+    maxHeight: undefined,
+    minHeight: undefined,
+    width: undefined,
+    maxWidth: undefined,
+    minWidth: undefined,
+});
+
+function unit(value: string | number | null | undefined | boolean, uom = 'px'): string | undefined {
+    return value !== null
+        && value !== undefined
+        && value !== false
+        && isFinite(Number(value)) ? `${value}${uom}` : undefined;
+}
+
+function componentFromRegistry(key: string): Component | undefined {
+    try {
+        return inject<ComponentRegistry>(props.registry || 'indicators', registry)?.get(key);
+    } catch (e) {
+        // Ignore the error
+        return undefined;
+    }
+}
+
+const classes = computed(() => ({
+    'activity-indicator-center': props.center,
+    'activity-indicator-absolute': props.absolute,
+    [props.size && `activity-indicator-${props.size}`]: !!props.size,
+}));
+
+const style = computed(() => ({
+    width: unit(props.width),
+    maxWidth: unit(props.maxWidth),
+    minWidth: unit(props.minWidth),
+    height: unit(props.height),
+    maxHeight: unit(props.maxHeight),
+    minHeight: unit(props.minHeight),
+}));
+
+const component = computed((): Component | undefined => {
+    if (typeof props.type === 'string') {
+        return componentFromRegistry(props.type);
+    }
+    return toRaw(props.type);
+});
+</script>
+
+<template>
+    <div
+        class="activity-indicator"
+        :class="classes"
+        :style="style"
+    >
+        <div class="activity-indicator-content">
+        <Component
+            :is="component"
+            class="mx-auto"
+        />
+        </div>
+    </div>
+</template>
+
+
+<!-- <script lang="ts">
 import { ComponentRegistry } from '@vue-interface/component-registry';
 import { defineComponent, inject, toRaw } from 'vue';
 import { registry } from './registry';
@@ -133,13 +220,9 @@ export default defineComponent({
             </div> 
         </div>
     </div>
-</template>
+</template> -->
 
 <style>
-.activity-indicator-label {
-    margin-top: .75rem;
-    text-align: center;
-}
 .activity-indicator-xs {
     font-size: .5rem;
 }
