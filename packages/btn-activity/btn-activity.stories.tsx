648dc14118c2e3d7190e5161df2523186a5fa740
Andrew-Knt <jak@jandrewknight.com>
2025-07-01 10:41:46 -0400

    fix: added controllable story to btn-activity

--- packages/btn-activity/btn-activity.stories.tsx
+++ packages/btn-activity/btn-activity.stories.tsx
@@ -1,12 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import { Dots, Spinner, Pulse } from '@vue-interface/activity-indicator';
-import { defineComponent, ref, onMounted, computed } from 'vue';
+import * as Indicators from '@vue-interface/activity-indicator';
+import { defineComponent, ref, onMounted, computed, markRaw } from 'vue';
 import './index.css';
 import BtnActivity from './src/BtnActivity.vue';
  
 const meta = {
     title: 'Example/Buttons/Button Activity',
     tags: ['autodocs'],
+    component: BtnActivity,
 } satisfies Meta;
 
 export default meta;
@@ -18,24 +19,97 @@ const handleActivityClick = (_event: MouseEvent, { toggle }: { toggle: () => voi
   setTimeout(() => toggle(), 2000);
 };
 
+export const ButtonActivityControl = {
+    args: {
+        activity: false,
+        indicator: Indicators.Dots,
+        indicatorSize: 'xs',
+        label: 'Dots',
+        orientation: 'right',
+        variant: 'btn-primary',
+        size: 'btn-md',
+        block: false,
+        disabled: false,
+    },
+    argTypes: {
+        activity: { control: 'boolean' },
+        indicator: { 
+            control: 'select', 
+            options: [
+                'Dots',
+                'Chase',
+                'Spinner',
+                'Square',
+                'SquareOrbit',
+                'SquareFold',
+                'CircleFade',
+                'CircleOrbit',
+                'CircleTrail',
+                'Pulse',
+                'DoublePulse',
+                'Grid',
+                'Facebook',
+                'Spotify',
+            ],
+            mapping: {
+                'Dots': markRaw(Indicators.Dots),
+                'Chase': markRaw(Indicators.Chase),
+                'Spinner': markRaw(Indicators.Spinner),
+                'Square': markRaw(Indicators.Square),
+                'SquareOrbit': markRaw(Indicators.SquareOrbit),
+                'SquareFold': markRaw(Indicators.SquareFold),
+                'CircleFade': markRaw(Indicators.CircleFade),
+                'CircleOrbit': markRaw(Indicators.CircleOrbit),
+                'CircleTrail': markRaw(Indicators.CircleTrail),
+                'Pulse': markRaw(Indicators.Pulse),
+                'DoublePulse': markRaw(Indicators.DoublePulse),
+                'Grid': markRaw(Indicators.Grid),
+                'Facebook': markRaw(Indicators.Facebook),
+                'Spotify': markRaw(Indicators.Spotify),
+            },
+        },
+        indicatorSize: {
+            control: 'select',
+            options: ['2xs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl'],
+        },
+        label: { control: 'text' },
+        orientation: { 
+            control: 'radio', 
+            options: ['top', 'bottom', 'left', 'right'] 
+        },
+        variant: { control: 'text' },
+        size: { control: 'text' },
+        block: { control: 'boolean' },
+        disabled: { control: 'boolean' },
+    },
+    render: (args) => {
+        return () => (
+            <div class="mb-5 flex gap-2">
+                <BtnActivity {...args} onClick={handleActivityClick}></BtnActivity>
+            </div>
+        );
+    }
+} satisfies Story;
+
+
 export const ActivityTypes = {
     name: 'Activity Types',
     render: () => (
         <div class="mb-5 flex gap-2">
-            <BtnActivity indicator={Dots} onClick={handleActivityClick}>Dots</BtnActivity>
-            <BtnActivity indicator={Spinner} onClick={handleActivityClick}>Spinner</BtnActivity>
-            <BtnActivity indicator={Pulse} onClick={handleActivityClick}>Pulse</BtnActivity>
+            <BtnActivity indicator={Indicators.Dots} onClick={handleActivityClick}>Dots</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} onClick={handleActivityClick}>Spinner</BtnActivity>
+            <BtnActivity indicator={Indicators.Pulse} onClick={handleActivityClick}>Pulse</BtnActivity>
         </div>
     ),
-} satisfies Story;
+};
 
 export const DotsSizes = {
     name: 'Dots Sizes',
     render: () => (
         <div class="mb-5 flex gap-2">
-            <div><BtnActivity indicator={Dots} size="btn-sm" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
-            <div><BtnActivity indicator={Dots} size="btn-md" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
-            <div><BtnActivity indicator={Dots} size="btn-lg" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Dots} size="btn-sm" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Dots} size="btn-md" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Dots} size="btn-lg" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
         </div>
     ),
 };
@@ -44,9 +118,9 @@ export const SpinnerSizes = {
     name: 'Spinner Sizes',
     render: () => (
         <div class="mb-5 flex gap-2">
-            <div><BtnActivity indicator={Spinner} size="btn-sm" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
-            <div><BtnActivity indicator={Spinner} size="btn-md" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
-            <div><BtnActivity indicator={Spinner} size="btn-lg" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Spinner} size="btn-sm" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Spinner} size="btn-md" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Spinner} size="btn-lg" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
         </div>
     ),
 };
@@ -54,9 +128,9 @@ export const PulseSizes = {
     name: 'Pulse Sizes',
     render: () => (
         <div class="mb-5 flex gap-2">
-            <div><BtnActivity indicator={Pulse} size="btn-sm" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
-            <div><BtnActivity indicator={Pulse} size="btn-md" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
-            <div><BtnActivity indicator={Pulse} size="btn-lg" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Pulse} size="btn-sm" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Pulse} size="btn-md" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
+            <div><BtnActivity indicator={Indicators.Pulse} size="btn-lg" onClick={handleActivityClick}>Click Me!</BtnActivity></div>
         </div>
     ),
 };
@@ -65,10 +139,10 @@ export const Orientation = {
     name: 'Orientation',
     render: () => (
         <div class="mb-5 flex gap-2">
-            <BtnActivity indicator={Spinner} orientation='top' onClick={handleActivityClick}>Orientation Top</BtnActivity>
-            <BtnActivity indicator={Spinner} orientation='bottom' onClick={handleActivityClick}>Orientation Bottom</BtnActivity>
-            <BtnActivity indicator={Spinner} orientation='left' onClick={handleActivityClick}>Orientation Left</BtnActivity>
-            <BtnActivity indicator={Spinner} orientation='right' onClick={handleActivityClick}>Orientation Right</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} orientation='top' onClick={handleActivityClick}>Orientation Top</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} orientation='bottom' onClick={handleActivityClick}>Orientation Bottom</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} orientation='left' onClick={handleActivityClick}>Orientation Left</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} orientation='right' onClick={handleActivityClick}>Orientation Right</BtnActivity>
         </div>
     ),
 };
@@ -77,10 +151,10 @@ export const BlockOrientation = {
     name: 'Block Orientation',
     render: () => (
         <div class="mb-5 flex-col gap-2">
-            <BtnActivity indicator={Spinner} block orientation='top' onClick={handleActivityClick}>Spinner Top</BtnActivity>
-            <BtnActivity indicator={Spinner} block orientation='bottom' onClick={handleActivityClick}>Spinner Bottom</BtnActivity>
-            <BtnActivity indicator={Spinner} block orientation='left' onClick={handleActivityClick}>Spinner Left</BtnActivity>
-            <BtnActivity indicator={Spinner} block orientation='right' onClick={handleActivityClick}>Spinner Right</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} block orientation='top' onClick={handleActivityClick}>Spinner Top</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} block orientation='bottom' onClick={handleActivityClick}>Spinner Bottom</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} block orientation='left' onClick={handleActivityClick}>Spinner Left</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} block orientation='right' onClick={handleActivityClick}>Spinner Right</BtnActivity>
         </div>
     ),
 };
@@ -89,10 +163,10 @@ export const Variants = {
     name: 'Variants',
     render: () => (
         <div class="mb-5 flex gap-2">
-            <BtnActivity indicator={Spinner} variant="btn-secondary" onClick={handleActivityClick}>btn-secondary</BtnActivity>
-            <BtnActivity indicator={Spinner} variant="btn-warning" onClick={handleActivityClick}>btn-warning</BtnActivity>
-            <BtnActivity indicator={Spinner} variant="btn-danger" onClick={handleActivityClick}>btn-danger</BtnActivity>
-            <BtnActivity indicator={Spinner} variant="btn-success" onClick={handleActivityClick}>btn-success</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} variant="btn-secondary" onClick={handleActivityClick}>btn-secondary</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} variant="btn-warning" onClick={handleActivityClick}>btn-warning</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} variant="btn-danger" onClick={handleActivityClick}>btn-danger</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} variant="btn-success" onClick={handleActivityClick}>btn-success</BtnActivity>
         </div>
     ),
 };
@@ -113,10 +187,10 @@ export const ActivityAttribute = {
               
                 return () => (
                     <div class="flex gap-2">
-                        <BtnActivity indicator={Spinner} activity={activity.value}>
+                        <BtnActivity indicator={Indicators.Spinner} activity={activity.value}>
                             {text.value}
                         </BtnActivity>
-                        <BtnActivity indicator={Spinner} activity={activity.value} disabled>
+                        <BtnActivity indicator={Indicators.Spinner} activity={activity.value} disabled>
                             {text.value}
                         </BtnActivity>
                     </div>
@@ -129,14 +203,14 @@ export const ActivityAttribute = {
 export const Disabled = {
     name: 'Disabled',
     render: () => (
-        <BtnActivity indicator={Spinner} disabled onClick={handleActivityClick}>Spinner Top</BtnActivity>
+        <BtnActivity indicator={Indicators.Spinner} disabled onClick={handleActivityClick}>Spinner Top</BtnActivity>
     ),
 };
 
 export const LabelAttribute = {
     name: 'Label Attribute',
     render: () => (
-        <BtnActivity indicator={Spinner} label="Some Text Here" onClick={handleActivityClick}></BtnActivity>
+        <BtnActivity indicator={Indicators.Spinner} label="Some Text Here" onClick={handleActivityClick}></BtnActivity>
     ),
 };
 
@@ -144,8 +218,8 @@ export const CustomElement = {
     name: 'Custom Element',
     render: () => (
         <div class="flex gap-2">    
-            <BtnActivity indicator={Spinner} onClick={handleActivityClick}>Label</BtnActivity>
-            <BtnActivity indicator={Spinner} label="Some Text Here" disabled onClick={handleActivityClick}></BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} onClick={handleActivityClick}>Label</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} label="Some Text Here" disabled onClick={handleActivityClick}></BtnActivity>
         </div>
     ),
 };
@@ -154,8 +228,8 @@ export const ClickEvents = {
     name: 'Click Events',
     render: () => (    
         <div>    
-            <BtnActivity indicator={Spinner} class="btn-primary mr-1" onClick={handleActivityClick}>Enabled</BtnActivity>
-            <BtnActivity indicator={Spinner} disabled onClick={handleActivityClick}>Disabled</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} class="btn-primary mr-1" onClick={handleActivityClick}>Enabled</BtnActivity>
+            <BtnActivity indicator={Indicators.Spinner} disabled onClick={handleActivityClick}>Disabled</BtnActivity>
         </div>
     ),
 };
