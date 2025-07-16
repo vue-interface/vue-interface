b3bf71f496e1df1bdddeee82b37a5710586562d7
Justin Kimbrell <jkimbrell@actengag.com>
2025-07-15 18:28:42 -0600

    chore: removed legaxy stuff

--- packages/form-control/index.ts
+++ packages/form-control/index.ts
@@ -1,11 +1,9 @@
-import config from './src/config.js';
 import FormControlErrors from './src/FormControlErrors.vue';
 import FormControlFeedback from './src/FormControlFeedback.vue';
 
 export * from './src/useFormControl.js';
 
 export {
-    config,
     FormControlErrors,
     FormControlFeedback
 };
