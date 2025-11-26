0b145efcb700d23d511b812ffe265c9be6e9f1e2
Andrew-Knt <jak@jandrewknight.com>
2025-11-25 23:47:19 -0500

    chore: added breadcrumb starter files

---
+++ packages/breadcrumb/vite.config.js
@@ -0,0 +1,46 @@
+import tailwindcss from '@tailwindcss/vite';
+import { pascalCase } from 'change-case';
+import path from 'path';
+import { defineConfig } from 'vite';
+import pkg from './package.json';
+
+const fileName = pkg.name.split('/')[1];
+
+const external = [
+    ...(pkg.dependencies ? Object.keys(pkg.dependencies) : []),
+    ...(pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : [])
+];
+
+export default ({ command }) => defineConfig({
+    build: {
+        sourcemap: command === 'build',
+        lib: {
+            entry: path.resolve(__dirname, 'index.ts'),
+            name: pascalCase(fileName),
+            fileName,
+        },
+        rollupOptions: {
+            external,
+            output: {
+                globals: external.reduce((carry, dep) => {
+                    return Object.assign(carry, {
+                        [dep]: pascalCase(dep)
+                    });
+                }, {}),
+            }
+        },
+        watch: !process.env.NODE_ENV && {
+            include: [
+                './tailwindcss/**/*.js'
+            ]
+        }
+    },
+    resolve: {
+        conditions: process.env.NODE_ENV === 'development' 
+            ? ['source', 'import', 'module', 'browser', 'default']
+            : ['import', 'module', 'browser', 'default']
+    },
+    plugins: [
+        tailwindcss()
+    ]
+});
